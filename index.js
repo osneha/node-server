20b4b66aec0147aa8fed3ec7c6185240e3f55ed7
Sneha Ojha <42754345+osneha@users.noreply.github.com>
2020-05-06 15:12:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const osneha = '127.0.0.1';
-const port = 3000;
+const port = 3002;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -9,6 +9,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello World');
 });
 
-server.listen(port, osneha, () => {
-  console.log(`Server running at http://${osneha}:${port}/`);
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
